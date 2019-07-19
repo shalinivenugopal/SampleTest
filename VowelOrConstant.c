652c5258aa5414d54fe9a6aba3a886b604cac3a1
shalinivenugopal <39789520+shalinivenugopal@users.noreply.github.com>
2019-07-19 04:14:23 -0700

    Update VowelOrConstant.c

--- VowelOrConstant.c
+++ VowelOrConstant.c
@@ -1,18 +1,10 @@
-#include <stdio.h>
-
-int main()
-{
-    char ch;
-
-    printf("Please Enter an alphabet: \n");
-    scanf(" %c", &ch);
-    
-    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
-		ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')  {
-		printf("\n %c is a VOWEL.", ch);
-	}
-    else {
-    	printf("\n %c is a CONSONANT.", ch);
-	}
-    return 0;
-}
+#include<stdio.h>
+#include<conio.h>
+int main(){
+char ch;
+	if(ch == 'a' || ch == 'e' || ch=='i' ||ch=='o' || ch=='u' ||ch=='A' || ch == 'E' || ch=='I' || ch=='O' || ch == 'U'
+	   printf("Vowel");
+	else
+	   printf("Consonant");
+return 0;	   
+ }
